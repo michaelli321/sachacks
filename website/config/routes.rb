595e30b7fae32882743ad96d5f7ffcd9be33a2eb
Michael Li <li.michael1998@gmail.com>
2018-11-17 22:47:43 -0800

    change everything lmao

--- website/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-	root 'application#index'
-	get 'index' => 'application#index'
-	get 'about' => 'application#about'
-	get 'data' => 'application#data'
-	get 'models' => 'application#models'
-end
